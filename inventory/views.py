c41a9a44324e67b120ac51a4653836a0c2e0128f
Bliss31415 <your_email@example.com>
2025-03-25 18:48:41 +0500

    need to complete add_invent

--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from inventory.models import Inventarnik, Inventory_status
 
 def invent_list(request):
@@ -11,3 +11,26 @@ def invent_list(request):
         'all_invent_status' : all_invent_status
     }
     return render(request, 'inventory/invent_list.html', context)
+
+def add_new_invent(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        inventory_number = request.POST.get('inventory_number')
+        registration_date = request.POST.get('registration_date')
+        photo = request.POST.get('photo')
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        location = request.POST.get('location')
+
+        if name and inventory_number and registration_date and photo and description and status and location:
+            Inventarnik.objects.create(
+                name=name,
+                inventory_number=inventory_number,
+                registration_date=registration_date,
+                photo=photo,
+                description=description,
+                status=status,
+                location=location
+            )
+        return redirect('inventory/invent_list.html')
+    return render(request,'inventory/add_invent.html')
